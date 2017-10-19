bf3c0436e83652746072bfcdc3a8ff28d603388f
xueliugit <liuxue4503@live.cn>
2017-10-19 15:57:00 +0800

    安全访问控件第一例
    winform下面线程间安全访问控件的第一个实现，利用SynchronizationContext的post方法，异步实现控制修改。

---
+++ multithread01/multithread01/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace multithread01
+{
+    public partial class Form1 : Form
+    {
+        SynchronizationContext SyncContext = null;
+        Thread thread1 = null;
+        public Form1()
+        {
+            InitializeComponent();
+            SyncContext = SynchronizationContext.Current;
+        }
+
+        private void change(object str)
+        {
+            this.label1.Text = str.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            thread1 = new Thread(new ParameterizedThreadStart(change2));
+            thread1.IsBackground = true;
+            thread1.Start("hello");
+        }
+
+        private void change2(object str)
+        {
+            SyncContext.Post(change, str);
+        }
+    }
+}
